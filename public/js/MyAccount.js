03e221b8e617e50ac559359e365c6735575304c9
Shivi Jain <shivijain2807@gmail.com>
2016-04-26 21:18:30 -0700

    profile completion status is now functional

--- public/js/MyAccount.js
+++ public/js/MyAccount.js
@@ -17,9 +17,22 @@ function initializeMyAccount($scope, $http) {
 //	$scope.formData = {};
 
 	var service = MyAccountService($http);
+	var count = 0;
+	var total =0;
 	service.get()
 		.success(function(data) {
 			$scope.userInfo = data;
+
+			for(var key in data){
+				if(data[key] != null && data[key] != ""){
+					count += 1;
+				}
+				total += 1;
+			}
+		$scope.userInfo.complete = Math.round((count*100)/total);
+		$(".progress-bar").animate({
+			width : $scope.userInfo.complete + "%"
+		}, 1000);
 		});
 	
 	$scope.updateInfo = function() {
@@ -28,13 +41,16 @@ function initializeMyAccount($scope, $http) {
 		// if form is empty, nothing will happen
 		if ($scope.userInfo.firstname != undefined) {
 			$scope.loading = true;
-			debugger;
+			
 			// call the create function from our service (returns a promise object)
 			service.create($scope.userInfo)
 
 				.success(function(data) {
 					console.log(data);
+
+					
 				});
+				
 		}
 	};
 }
