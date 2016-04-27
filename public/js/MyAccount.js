38d9a37ed957557c6e550c59ee3872d7ef216342
Shivi Jain <shivijain2807@gmail.com>
2016-04-26 22:26:44 -0700

    gauge graph ui

--- public/js/MyAccount.js
+++ public/js/MyAccount.js
@@ -19,9 +19,13 @@ function initializeMyAccount($scope, $http) {
 	var service = MyAccountService($http);
 	var count = 0;
 	var total =0;
+	
+	
+
 	service.get()
 		.success(function(data) {
 			$scope.userInfo = data;
+			$scope.userInfo.counter = data.counter;
 
 			for(var key in data){
 				if(data[key] != null && data[key] != ""){
@@ -29,6 +33,8 @@ function initializeMyAccount($scope, $http) {
 				}
 				total += 1;
 			}
+
+		activitygauge();
 		$scope.userInfo.complete = Math.round((count*100)/total);
 		$(".progress-bar").animate({
 			width : $scope.userInfo.complete + "%"
@@ -55,4 +61,112 @@ function initializeMyAccount($scope, $http) {
 	};
 }
 
+function activitygauge() {
+	    var gaugeOptions = {
 
+        chart: {
+            type: 'solidgauge'
+        },
+
+        title: null,
+
+        pane: {
+            center: ['50%', '85%'],
+            size: '140%',
+            startAngle: -90,
+            endAngle: 90,
+            background: {
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
+            }
+        },
+
+        tooltip: {
+            enabled: false
+        },
+
+        // the value axis
+        yAxis: {
+            stops: [
+                [0.1, '#55BF3B'], // green
+                [0.5, '#DDDF0D'], // yellow
+                [0.9, '#DF5353'] // red
+            ],
+            lineWidth: 0,
+            minorTickInterval: null,
+            tickPixelInterval: 400,
+            tickWidth: 0,
+            title: {
+                y: -70
+            },
+            labels: {
+                y: 16
+            }
+        },
+
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 5,
+                    borderWidth: 0,
+                    useHTML: true
+                }
+            }
+        }
+    };
+
+    // The speed gauge
+    $('#activity-gauge').highcharts(Highcharts.merge(gaugeOptions, {
+        yAxis: {
+            min: 0,
+            max: 200,
+            title: {
+                text: 'Visits'
+            }
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        series: [{
+            name: 'Visits',
+            data: [80],
+            dataLabels: {
+                format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/></div>'
+            },
+            tooltip: {
+                valueSuffix: ''
+            }
+        }]
+
+    }));
+
+
+
+    // Bring life to the dials
+    setTimeout(function () {
+        // Speed
+        var chart = $('#activity-gauge').highcharts(),
+            point,
+            newVal,
+            inc;
+
+        if (chart) {
+            point = chart.series[0].points[0];
+            inc = Math.round((Math.random() - 0.5) * 100);
+            newVal = point.y + inc;
+            //newVal = $scope.userInfo.counter;
+
+            if (newVal < 0 || newVal > 200) {
+                newVal = point.y - inc;
+            }
+
+            point.update(newVal);
+        }
+
+    }, 200);
+}
