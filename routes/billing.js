e128a50c9efcf4beb5c0e778a942bd38a5b7bbdf
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2016-05-04 01:50:39 -0700

    remove unwanted code

--- routes/billing.js
+++ routes/billing.js
@@ -2,7 +2,6 @@ var mysql = require('./mysql');
 var moment = require('moment');
 
 exports.addPayment = function(req, res){
-	//Hashing the entered password to store in the DB
 	var payment_id = req.param("user_id") + moment().unix();
 	console.log(req.body);
 	var insertReview = "insert into payment (paymentid,name,creditcard,cvv,billingaddress,month,year,userid) values ('"
