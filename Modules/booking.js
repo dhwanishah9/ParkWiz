06692002146268ba4daa99106e9be716d67a8fe6
Shivi Jain <shivijain2807@gmail.com>
2016-05-03 23:44:31 -0700

    updated queries for last 2 bookings/spaces & all bookings

--- Modules/booking.js
+++ Modules/booking.js
@@ -9,7 +9,7 @@ exports.list = function(req, res){
 exports.gettopbookings = function(userid, res) {	
 	//var getBooking = "SELECT CONCAT(s.addressline1, \" \", s.addressline2) AS address, r.status FROM spot s JOIN (SELECT spotid, status, startdate from reservation where userid ="+ userid + ") r WHERE s.spotid = r.spotid ORDER BY r.startdate DESC LIMIT 2";
 	debugger;
-	var getBooking = "SELECT CONCAT(s.addressline1, \" \", s.addressline2) AS address, r.status,u.monthlypending, u.pendingtransfer, u.lifetimeearnings, u.lifetimetransactions FROM spot s JOIN (SELECT spotid, status, startdate from reservation where userid ="+ userid + ") r JOIN user u ON u.userid = "+ userid + " WHERE s.spotid = r.spotid ORDER BY r.startdate DESC LIMIT 2";
+	var getBooking = "SELECT CONCAT(s.addressline1, \" \", s.addressline2) AS address, r.status,u.monthlypending, u.pendingtransfer, u.lifetimeearnings, u.lifetimetransactions FROM spot s JOIN (SELECT spotid, status, startdate,userid from reservation where userid ="+ userid + ") r JOIN user u ON u.userid = r.userid WHERE s.userid = "+ userid + " ORDER BY r.startdate DESC LIMIT 2";
 	console.log("Query is:" + getBooking);
 	
 	//Calling the fetch method using mysql module
@@ -33,7 +33,7 @@ exports.getallbookings = function(userid, res) {
 	
 	//var getBooking = "SELECT CONCAT(s.addressline1, \" \", s.addressline2) AS address, r.status FROM spot s JOIN ( SELECT spotid, status, startdate from reservation where userid ='"+ userid + "') r WHERE s.spotid = r.spotid ORDER BY r.startdate DESC";
 
-	var getBooking = "SELECT CONCAT(s.addressline1, \" \", s.addressline2) AS address, r.status,i.imageurl, u.monthlypending, u.pendingtransfer, u.lifetimeearnings, u.lifetimetransactions FROM spot s JOIN image i ON i.spotid = s.spotid JOIN reservation r ON s.spotid = r.spotid JOIN user u ON u.userid = "+ userid + " WHERE r.userid = "+ userid + " ORDER BY r.startdate DESC";
+	var getBooking = "SELECT DISTINCT CONCAT(s.addressline1, \" \", s.addressline2) AS address, r.status,i.imageurl, u.monthlypending, u.pendingtransfer, u.lifetimeearnings, u.lifetimetransactions FROM spot s JOIN image i ON i.spotid = s.spotid JOIN reservation r ON r.userid = s.userid JOIN user u ON u.userid = r.userid WHERE r.userid = "+ userid + " ORDER BY r.startdate DESC";
 	console.log("Query is:" + getBooking);
 	
 	debugger;
@@ -53,7 +53,7 @@ exports.getallbookings = function(userid, res) {
 };
 
 exports.getspaces = function(userid, res) {	
-	var getBooking = "SELECT CONCAT(s.addressline1, \" \", s.addressline2) AS address, s.listingtype, r.status FROM spot s JOIN (SELECT spotid, status, startdate from reservation where userid ="+ userid + ") r WHERE s.spotid = r.spotid ORDER BY r.startdate DESC LIMIT 2";
+	var getBooking = "SELECT CONCAT(s.addressline1, \" \", s.addressline2) AS address, s.listingtype, r.status FROM spot s JOIN (SELECT spotid, status, startdate,userid from reservation where userid ="+ userid + ") r WHERE r.userid = s.userid ORDER BY r.startdate DESC LIMIT 2";
 	console.log("Query is:" + getBooking);
 	
 	//Calling the fetch method using mysql module
